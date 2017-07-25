17ecd4dac7d9269a0422ad864c59cc3f82830834
Johan Oelrich <johan.oelrich@live.se>
2017-07-25 15:56:39 +0200

    Same as previous

--- src/components/otk.ts
+++ src/components/otk.ts
@@ -9,7 +9,7 @@ export var DEFAULT_SAMPLE_SIZE = 65536;
 export class otk {
   private _sequence: string;
   private _stats: stats.Stats;
-
+  private _method: string;
   private _domain_size: number;
   private _sample_size: number;
   private _sample_process_time: number;
@@ -30,18 +30,22 @@ export class otk {
   }
 
   private populate_stats(sample_size:number = this._sample_size) {
-    if(this._domain_size == 1) {
-      this._stats = stats.get_single_stats(this._sequence, this._tm_type);
-    } else if(this._domain_size > this._sample_size) {
-      this._stats = stats.get_stats(this._sequence, this._sample_size, this._tm_type);
+    var sequence_generator : IterableIterator<string>;
+    if(this._domain_size > this._sample_size) {
+      sequence_generator = stats.random_sequences(this._sequence, sample_size);
+      this._method = "random";
     } else {
-      this._stats = stats.get_full_stats(this._sequence, this._tm_type);
+      sequence_generator = stats.all_sequences(this._sequence);
+      this._method = "all";
     }
+    this._stats = stats.get_stats(sequence_generator, this._tm_type);
   }
 
   public size() { return this._domain_size; }
 
   public time() { return this._sample_process_time; }
+
+  public method() { return this._method; }
 
   public sequence(sequence: string | null) {
     if(sequence == null) {
