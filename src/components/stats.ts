27bf597ec1547136c90f8adb8f315f0ae41b288f
Johan Oelrich <johan.oelrich@live.se>
2017-07-25 15:52:44 +0200

    Better stats
    Stats now provides two generator functions that generate flat sequences
    from an IUPAC-sequence. Randomly or sequentially. Stats also provide a
    method to extract statistics regarding GC-content and Tm for
    IUPAC-sequences.

--- src/components/stats.ts
+++ src/components/stats.ts
@@ -1,7 +1,6 @@
 import { gc_content as gcr } from "./gc-content";
 import * as meltingpoint from "./meltingpoint";
 import * as IUPAC from "./iupac";
-
 import * as D3 from "d3";
 
 interface DataPoint {
@@ -18,19 +17,69 @@ export interface Statistics {
   deviation?: number;
   mean?: number;
   median?: number;
+  ext?: { min: number, max: number};
+  count: number;
   exact?:number;
 }
 
-export function get_stats(sequence: string, sample_size: number, tmtype:meltingpoint.TmType): Stats {
-  return {gc:{exact:0},tm:{exact:0}};
+function stats(stats:number[]) : Statistics {
+  if(stats.length > 1) {
+    var ext = D3.extent(stats);
+    var gc: Statistics = {
+      deviation: D3.deviation(stats),
+      ext: {min: ext[0], max: ext[1]},
+      mean: D3.mean(stats),
+      median: D3.median(stats),
+      count: stats.length
+    };
+    return gc;
+  }
+  if(stats.length == 1) {
+    return { exact: stats[0] , count: 1 };
+  }
+  throw new Error("Stats: Undefined for empty data set.")
 }
 
-export function get_full_stats(sequence: string, tmtype:meltingpoint.TmType): Stats {
-  return {gc:{exact:0},tm:{exact:0}};
+function next_selection(domain_vars: number[], selection: number[]) : number[] {
+  for (var idx = 0; idx < selection.length; idx++) {
+    var next = selection[idx] + 1;
+    if(next < domain_vars[idx]) {
+      selection[idx] = next;
+      break;
+    } else {
+      selection[idx] = 0;
+    }
+  }
+  return selection;
 }
 
-export function get_single_stats(sequence: string, tmtype:meltingpoint.TmType): Stats {
-  return {gc:{exact:gcr(sequence)},tm:{exact:meltingpoint.Tm(sequence, tmtype)}};
+export function* all_sequences(sequence: string) {
+  var domain = sequence.split("").map((base) => { return IUPAC.expand(base); });
+  var domain_vars = domain.map((base) => { return base.length; });
+  var domain_size = domain_vars.reduce((sum, value) => { return sum * value; }, 1);
+  var sequence_selection = Array(domain_vars.length).fill(0);
+  for (var idx = 0; idx < domain_size; idx++) {
+    var flat_sequence = sequence_selection.map((selected,idx) => { return domain[idx][selected]; }).join("");
+    sequence_selection = next_selection(domain_vars, sequence_selection);
+    yield flat_sequence;
+  }
+}
+
+export function* random_sequences(sequence: string, sample_size: number) {
+  var domain = sequence.split("").map((base) => { return IUPAC.expand(base); });
+  for (var idx = 0; idx < sample_size; idx++) {
+    yield domain.map((bases) => { return bases[Math.floor(D3.randomUniform(0, bases.length - 1)())]; }).join("");
+  }
+}
+
+export function get_stats(generator:IterableIterator<string>, tmtype: meltingpoint.TmType) : Stats {
+  var data: DataPoint[] = [];
+  for (let flat_sequence of generator) {
+    data.push({gc:gcr(flat_sequence), tm:meltingpoint.Tm(flat_sequence, tmtype)});
+  }
+  return {
+    gc:stats(data.map((point) => { return point.gc; })),
+    tm: stats(data.map((point) => { return point.tm; }))};
 }
 
 export function expanded_size(sequence: string) {
@@ -38,23 +87,4 @@ export function expanded_size(sequence: string) {
     .split("")
     .map((base) => { return IUPAC.expand(base).length; })
     .reduce((sum, value) => { return sum * value; }, 1); 
-}
-
-function random_selection(sequence: string, size: number) {
-  if(sequence.length < 1) {
-    throw new Error("Statistics:random_selection: no sequence");
-  }
-  var domain = sequence.split("").map((base) => { return IUPAC.expand(base); });
-
-  var selected = D3.set();
-  while (selected.size() < size) {
-    selected.add(
-      domain.map((bases) => {
-        if(bases.length == 1) {
-          return bases[0];
-        }
-        return  bases[D3.randomUniform(0, bases.length - 1)()]; }).join()
-    );
-  }
-  return selected;
 }
--- src/test/stats.ts
+++ src/test/stats.ts
@@ -1,19 +1,55 @@
 import { suite, test, timeout, slow } from "mocha-typescript";
 import { assert, expect } from "chai";
-import * as statistics from "../components/stats";
+import * as stats from "../components/stats";
 
-@suite("Statistics (Expansion)", slow(1000), timeout(2000))
-class SuiteExpansionSize {
+@suite("Statistics (Domain size)", slow(1000), timeout(2000))
+class SuiteDomainSize {
     @test "TAN expands to 4 sequences"() {
-      expect(statistics.expanded_size("TAN")).to.equal(4);
+      expect(stats.expanded_size("TAN")).to.equal(4);
     }
     @test "VAN expands to 12 sequences"() {
-      expect(statistics.expanded_size("VAN")).to.equal(12);
+      expect(stats.expanded_size("VAN")).to.equal(12);
     }
     @test "N expands to 4 sequences"() {
-      expect(statistics.expanded_size("N")).to.equal(4);
+      expect(stats.expanded_size("N")).to.equal(4);
     }
     @test "NNNNNNNN expands to 65 536 sequences"() {
-      expect(statistics.expanded_size("NNNNNNNN")).to.equal(65536);
+      expect(stats.expanded_size("NNNNNNNN")).to.equal(65536);
+    }
+    @test "N{128} expands to 4^128 sequences"() {
+      expect(stats.expanded_size("NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN")).to.equal(Math.pow(4,128));
+    }
+}
+@suite("Statistics (Expansion)", slow(1000), timeout(2000))
+class SuiteSequenceExpansion {
+    @test "TAN expands to 4 sequences"() {
+      var gen = stats.all_sequences("TAN");
+      var count = 0;
+      for (let flat_sequence of gen) {
+        expect(['TAA','TAT','TAG','TAC'].some((x) => x == flat_sequence)).to.be.true;
+        count++;
+      }
+      expect(count).to.equal(4);
+    }
+    @test "TAN expands to 8 random sequences"() {
+      var gen = stats.random_sequences("TAN", 8);
+      var count = 0;
+      for (let flat_sequence of gen) {
+        expect(['TAA','TAT','TAG','TAC'].some((x) => x == flat_sequence)).to.be.true;
+        count++;
+      }
+      expect(count).to.equal(8);
+    }
+    @test "NAN expands to 16 random sequences"() {
+      var gen = stats.random_sequences("NAN", 16);
+      var count = 0;
+      for (let flat_sequence of gen) {
+        expect(['AAA','AAT','AAG','AAC',
+                'TAA','TAT','TAG','TAC',
+                'GAA','GAT','GAG','GAC',
+                'CAA','CAT','CAG','CAC'].some((x) => x == flat_sequence)).to.be.true;
+        count++;
+      }
+      expect(count).to.equal(16);
     }
 }
